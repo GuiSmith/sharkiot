13234799dfb83a1e68c857bdd62ca8cca17ddde8
Guilherme Smith - Banco de Dados <guilherme.rodrigues@ixcsoft.com.br>
2025-05-15 17:24:33 -0300

    feat: header e botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const apiUrl = `http://localhost:8000`;
+  const sensorEndpoint = `sensor`;
+  const leituraEndpoint = `leitura`;
+
+  const [sensores, setSensores] = useState([]);
+  const [leituras, setLeituras] = useState([]);
+
+  // Buscando sensores
+  useEffect(() => {
+    fetch(`${apiUrl}/${sensorEndpoint}`)
+      .then(async response => {
+        if (response.status == 204) {
+          toast("Nenhum sensor encontrado");
+          return;
+        }
+
+        if (![200, 204].includes(response.status)) {
+          toast("Erro ao listar sensores");
+          return;
+        }
+
+        const sensores = await response.json();
+        setSensores(sensores);
+      })
+      .catch(error => {
+        console.debug(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (sensores.length == 0) return;
+
+    fetch(`${apiUrl}/${leituraEndpoint}`)
+      .then(async response => {
+        if (response.status == 204) {
+          toast("Nenhuma leitura encontrada");
+          return;
+        }
+
+        if (![200, 204].includes(response.status)) {
+          toast("Erro ao listar leituras");
+          return;
+        }
+
+        const leituras = await response.json();
+        console.log('Novas leituras: ', leituras);
+        setLeituras((prev) => [...prev, leituras]);
+      })
+  }, [sensores]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='mt-3'>
+      <article className='container alert alert-primary'>
+        <h1 className='text-center'>Sensores</h1>
+        <div className='row text-center mt-5'>
+          {/* Presença */}
+          <div className='col-lg-6 col-sm-12 col-md-12'>
+            <h4>Presença</h4>
+            <p style={{ textIndent: '1rem' }}>
+              Identifica se há algum objeto na direção em que o sensor aponta.
+            </p>
+          </div>
+          {/* Proximidade */}
+          <div className='col-lg-6 col-sm-12 col-md-12'>
+            <h4>Proximidade</h4>
+            <p style={{ textIndent: '1rem' }}>
+              Identifica a distância entre o sensor e o objeto, em centímetros.
+            </p>
+          </div>
+        </div>
+        <hr />
+        <div className='d-flex justify-content-around'>
+          {sensores.map((sensor, index) => (
+            <button key={index} className='btn btn-info'>
+              {sensor.tipo}
+            </button>
+          ))}
+        </div>
+      </article>
+      <main className='container mt-4'>
+        <h2 className='text-center'>Leituras</h2>
+        <table className='table table-stripped table-light'>
+          <thead>
+            {leituras.length}
+          </thead>
+          <tbody>
+            {leituras.length == 0
+              ? ''
+              : leituras.map((leitura, index) => (
+                <tr id={leitura.id} key={index}>
+
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </main>
+      <ToastContainer position="bottom-right" />
+    </section>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'; // ✅ Import here
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional
 import './index.css'
 import App from './App.jsx'
 
